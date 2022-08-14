fa611c47efa9e9727b4769a56e12e7b8e1c22166
hazer-hazer <gordeev.john@gmail.com>
2022-08-14 18:36:57 +0300

    Add color check and warning

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.raw())
 
 const hexColorToInt = c => [parseInt(c.substr(4, 2), 16), parseInt(c.substr(2, 2), 16), parseInt(c.substr(0, 2), 16)]
+const rgbArrToHex = ([r, g, b]) => `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`
 
 const yeelight = new Yeelight({ ip: '192.168.1.159', port: 55443 })
 
@@ -23,7 +24,15 @@ yeelight.set_rgb([30, 215, 96])
 app.post('/color', (req, res) => {
     const { color } = req.body
     console.log(`Set YeeLight color to ${color}`, chalk.bgHex(color).hex(color)('■'))
-    yeelight.set_rgb(hexColorToInt(color.slice(1)))
+    const rgbColor = hexColorToInt(color.slice(1))
+    const hexColor = rgbArrToHex(rgbArrToHex)
+    yeelight.set_rgb(rgbColor)
+    console.log(`Set color to rgb [${rgbColor.join(', ')}] as hex ${hexColor}`)
+    
+    if (hexColor !== color) {
+        console.warn(`Resulting color does not match color from response: ${hexColor} /vs/ ${color}`)
+    }
+    
     res.status(200)
     res.send({})
 })
