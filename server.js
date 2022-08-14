fe5068f43d93765f95dee8b2a529213053d54033
hazer-hazer <gordeev.john@gmail.com>
2022-08-14 17:33:52 +0300

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const chalk = require('chalk')
+const Yeelight = require('yeelight2')
+
+const app = express()
+const port = 8080
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.raw())
+
+const hexColorToInt = c => parseInt(c.substr(4, 2) + c.substr(2, 2) + c.substr(0, 2), 16)
+
+Yeelight.discover(bulb => {
+    console.log('YeeLight bulb found');
+
+    app.post('/color', (req, res) => {
+        const {color} = req.body
+        console.log(`Set YeeLight color to ${color}`, chalk.bgHex(color).hex(color)('■'))
+        bulb.set_rgb(hexColorToInt(color))
+        res.status(200)
+        res.send({})
+    })
+
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`)
+    })
+})
