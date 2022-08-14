b74e7f5cdee1a2b6602682fe2ea5ff7da5a98151
hazer-hazer <gordeev.john@gmail.com>
2022-08-14 18:01:56 +0300

    Use yeelight-node package

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const chalk = require('chalk')
-const Yeelight = require('yeelight2')
+
+const { Yeelight } = require('yeelight-node')
 
 const app = express()
 const port = 8080
@@ -12,20 +13,20 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(bodyParser.raw())
 
-const hexColorToInt = c => parseInt(c.substr(4, 2) + c.substr(2, 2) + c.substr(0, 2), 16)
+const hexColorToInt = c => [parseInt(c.substr(4, 2), 16), parseInt(c.substr(2, 2), 16), parseInt(c.substr(0, 2), 16)]
 
-Yeelight.discover(function (bulb) {
-    console.log('YeeLight bulb found');
+const yeelight = new Yeelight({ ip: '192.168.1.159', port: 55443 })
 
-    app.post('/color', (req, res) => {
-        const {color} = req.body
-        console.log(`Set YeeLight color to ${color}`, chalk.bgHex(color).hex(color)('■'))
-        bulb.set_rgb(hexColorToInt(color))
-        res.status(200)
-        res.send({})
-    })
+yeelight.set_power('on')
 
-    app.listen(port, () => {
-        console.log(`Listening on port ${port}`)
-    })
+app.post('/color', (req, res) => {
+    const { color } = req.body
+    console.log(`Set YeeLight color to ${color}`, chalk.bgHex(color).hex(color)('■'))
+    yeelight.set_rgb(hexColorToInt(color))
+    res.status(200)
+    res.send({})
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
